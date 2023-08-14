a6e2e257c08fe69f4cfb0d95251edc1384f584c2
mregor_787 <egor0288354@yandex.ru>
2023-08-15 02:30:47 +0300

    Fix ImageViewer, add Button

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -2,7 +2,7 @@ import { StyleSheet, Image } from 'react-native';
 
 export default function ImageViewer({ placeholderImageSource }) {
   return (
-    <Image source={PlaceholderImage} style={styles.image} />
+    <Image source={placeholderImageSource} style={styles.image} />
   );
 }
 
